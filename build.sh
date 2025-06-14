f837d4c6d083cdbe7618a72f1ee0d315260d9553
Elysia <39023210+elysia-best@users.noreply.github.com>
2025-06-14 17:32:12 +0800

    fix(build.sh): remove TODO comment

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ else
   git tag -d beta || true
   # Always true if there's no tag
   version=$(git describe --abbrev=0 --tags 2>/dev/null || echo "v0.0.0")
-  # TODO: Repleace this assets with our new frontend if needed
   webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/OpenListTeam/OpenList-Frontend/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 fi
 
