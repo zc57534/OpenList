e40485e32b2300b7da013cee5f103ccb97548b24
Elysia <39023210+elysia-best@users.noreply.github.com>
2025-06-14 17:43:49 +0800

    feat(build.sh): update GitAuthor

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 appName="openlist"
 builtAt="$(date +'%F %T %z')"
-gitAuthor="The OpenList Bot <bot@openlist.team>"
+gitAuthor="The OpenList Projects Contributors <noreply@openlist.team>"
 gitCommit=$(git log --pretty=format:"%h" -1)
 
 if [ "$1" = "dev" ]; then
